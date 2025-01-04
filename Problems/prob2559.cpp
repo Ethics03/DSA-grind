c01354069591d391bc541582a3af9f0d5ffdffb2
Rachit Srivastava <honey032004@gmail.com>
2025-01-04 21:35:40 +0530

    completed 2559

--- Problems/prob2559.cpp
+++ Problems/prob2559.cpp
@@ -2,22 +2,33 @@
 
 using namespace std;
 
-int main() {
-    vector<int> ans;
-        vector<char> vowels = {'a','e','i','o','u'};
-        vector<string> words = {"aba","eee","e","nsn","ege"};
-        vector<int> volc;
-        int i = 0;
-        for(int j = 0; j < words.size() ; j++){
-                if(find(vowels.begin(), vowels.end(), words[j][0]) != vowels.end() && find(vowels.begin(), vowels.end(), words[j][words[j].length()-1]) != vowels.end()){
-                            volc.push_back(j);
+vector<int> vowelStrings(vector<string> words, vector<vector<int>> queries) {
+        vector<int> ans;
+        int n = words.size();
+        unordered_set<char> vowels = {'a','e','i','o','u'};
+        vector<int> volc(n,0);
+        for(int j = 0; j < n ; j++){
+                if(vowels.count(words[j].front()) && vowels.count(words[j].back())){
+                            volc[j] = 1;
+                }
+                if(j > 0){
+                    volc[j] += volc[j-1];
                 }
         }
+        for(const auto q : queries){
+            int count = volc[q[1]]  - (q[0] > 0 ? volc[q[0]-1] : 0);
+            ans.push_back(count);
+        }
+        return ans;
+    }
 
-                for(int i = 0 ; i < volc.size() ; i++){
-                  cout << volc[i];
-                }
-        
+int main() {
+   vector<string> words =  {"aba","bcb","ece","ada"};
+   vector<vector<int>> queries = {{1,2},{0,3},{1,3}};
+   vector<int> ans = vowelStrings(words,queries);
+  for(int i = 0 ; i < ans.size(); i++){
+      cout << ans[i] << " " ;
+  }
 
     return 0;
 }

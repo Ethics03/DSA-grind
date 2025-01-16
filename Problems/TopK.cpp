69c01af17cfee97a5afc15b388c5aa2304f18352
Rachit Srivastava <honey032004@gmail.com>
2025-01-16 23:42:48 +0530

    neet: topk frequent

--- Problems/TopK.cpp
+++ Problems/TopK.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+vector<int> topKFrequent(vector<int> nums, int k) {
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        vector<pair<int, int>> arr;
+        for (const auto& p : count) {
+            arr.push_back({p.second, p.first});
+        }
+        sort(arr.rbegin(), arr.rend());
+
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(arr[i].second);
+        }
+        return res;
+    }
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+
+    
     return 0;
 }
     

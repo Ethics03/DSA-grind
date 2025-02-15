75439ed7aa18de405871e8fe644d7e92b33d2709
Rachit Srivastava <honey032004@gmail.com>
2025-02-15 22:18:05 +0530

    leet: valid paranthesis

---
+++ Problems/validParan.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ bool isValid(string s) {
+        stack<char> st;
+
+        for(char &ch : s){
+            if(ch == '(' || ch =='{' || ch == '['){
+                st.push(ch);
+            }
+        else{
+            if(st.empty()) return false;
+            if((ch == ')' && st.top() != '(') || (ch == ']' && st.top() != '[') || (ch == '}' && st.top() != '{')){
+                return false;
+            } 
+            st.pop();
+        }
+        }
+        return st.empty();
+    }
+
+int main() {
+  
+  string s = "{}()((}}";
+
+ cout << isValid(s);
+
+  return 0;
+}
+    
